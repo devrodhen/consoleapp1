00293d21cd515c48fa27c7c5f5b1c4b653614c11
Rodrigo Henrique <rod_hen@hotmail.com>
2020-02-04 16:52:46 -0300

    Created files

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x = new Triangulo();
+            x.A = 10;
+            x.B = 20;
+
+            Somar();
+        }
+    }
+}
---
+++ ConsoleApp1/Triangulo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+
+        static double Somar()
+        {
+            return A + B;
+        }
+    }
+}
